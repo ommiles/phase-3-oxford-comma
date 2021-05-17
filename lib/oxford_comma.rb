80c65c34768476de82a412a8b2dba3944b7a8269
OM Miles <miles@ommiles.com>
2021-05-17 11:49:04 -0700

    commit tree a1e7312ac53153c67e58c3847e21ed35e9dd7683

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    if array.size == 1 
+        array[0]
+    elsif array.size == 2
+        array.join(" and ")
+    elsif array.size >= 3
+        last_item = array.pop
+        array.join(", ") + ", and #{last_item}"
+    end
 end
